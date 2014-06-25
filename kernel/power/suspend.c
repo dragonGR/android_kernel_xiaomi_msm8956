e81875603363abebc2c0e0ad45854a64c4ffac18
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2014-06-24 23:38:08 -0400

    PM / Sleep: Remove ftrace_stop/start() from suspend and hibernate
    ftrace_stop() and ftrace_start() were added to the suspend and hibernate
    process because there was some function within the work flow that caused
    the system to reboot if it was traced. This function has recently been
    found (restore_processor_state()). Now there's no reason to disable
    function tracing while we are going into suspend or hibernate, which means
    that being able to trace this will help tremendously in debugging any
    issues with suspend or hibernate.
    
    This also means that the ftrace_stop/start() functions can be removed
    and simplify the function tracing code a bit.
    
    Link: http://lkml.kernel.org/r/1518201.VD9cU33jRU@vostro.rjw.lan
    
    Acked-by: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Reviewed-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -302,7 +302,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_wake;
 	}
 
-	ftrace_stop();
 	error = disable_nonboot_cpus();
 	if (error || suspend_test(TEST_CPUS)) {
 		log_suspend_abort_reason("Disabling non-boot cpus failed");
@@ -332,7 +331,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Enable_cpus:
 	enable_nonboot_cpus();
-	ftrace_start();
 
  Platform_wake:
 	platform_resume_noirq(state);
