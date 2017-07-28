56466e13b699d47bb9730e97a0a889e6d596dd7c
dragonGR <alextsanisbadlorg@gmail.com>
2017-07-28 23:17:26 +0000

    build: Update script
    > Prevent duplicated copy paste.
    > Remove derped/finished zips before building.
    > Remove TC STRIP since we no longer use it.
    > Added build date to final zip(Day/Month/Year)

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=../AnyKernel2
 DTBTOOL=$KERNEL_DIR/dtbTool
+DATE=$(date +"%d%m%Y")
 KERNEL_NAME="IceColdKernel"
-DEVICE="-kenzo"
-FINAL_ZIP="$KERNEL_NAME""$DEVICE".zip
+DEVICE="-kenzo-"
+FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE".zip
+
+if [ -f "$FINAL_ZIP" ]; then
+   rm -f $FINAL_ZIP
+fi
 
 export ARCH=arm64
 export KBUILD_BUILD_USER="Stark"
 export KBUILD_BUILD_HOST="StarkBoX"
 export CROSS_COMPILE="$HOME/toolchain/linaro-6.3/bin/aarch64-linux-gnu-"
 export LD_LIBRARY_PATH="$HOME/toolchain/linaro-6.3/lib/"
-STRIP="$HOME/toolchain/linaro-6.3/bin/aarch64-linux-gnu-strip"
 
 make mrproper > /dev/null 2>&1
 make lineageos_kenzo_defconfig > /dev/null 2>&1
-cp -arn arch/arm/boot/dts/* arch/arm64/boot/dts/ > /dev/null 2>&1
-cp -arn include/dt-bindings/* arch/arm64/include/ > /dev/null 2>&1
 make -j$(($(nproc --all) * 2))
 
 $DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
